f3d180a8fef520e2801ea797b070a24726dad3a5
unknown <espanou02@gmail.com>
2018-08-07 23:50:57 +0300

    Added an Array test Class

---
+++ src/dokimes/TestArrays.java
@@ -0,0 +1,15 @@
+package dokimes;
+
+public class TestArrays {
+	
+	public static void main(String[] args) {
+		String[] pets = {"Fido", "Zeus", "Bin"};	
+		int x = pets.length;
+		String s = pets[0]; // s is now the String "Fido"
+		s = s + " " + "is a dog"; // s is now "Fido is a dog"
+		System.out.println(s);
+		
+		
+	}
+
+}
